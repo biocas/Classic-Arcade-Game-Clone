a0120afab85e3a7fd633efc5253f9b9568a3b945
Margarida Salvado <margaridasalvado@192-168-1-11.tpgi.com.au>
2017-06-19 10:32:07 +0100

    trying to ckeckCollisions between Player and enemies

--- frontend-nanodegree-arcade-game/js/app.js
+++ frontend-nanodegree-arcade-game/js/app.js
@@ -1,8 +1,12 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = x; 
+    this.y = y; 
+    this.w = 80; 
+    this.h = 60;
+    this.speed = speed; 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +18,15 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt; 
+    
+     if(this.x > 505) {
+            this.x = 0;
+        }
+   if (this.checkCollisions()) {
+       console.log("collision checked");
+       }
+    
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +37,107 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(x, y) {
+    //Enemy.call(this); 
+    this.x = x; 
+    this.y = y; 
+    this.sprite = 'images/char-boy.png';
+    this.w = 101; 
+    this.h = 171;
+    
+    this.box = [this.x, this.y, this.w, this.h];
+};
 
+
+/* Player.prototype = Object.create(Enemy.prototype);
+Player.prototype.constructor = Player; */
+Player.prototype.update = function(dt) {
+    // makes sure player moves equally across browsers
+    this.x * (dt); 
+    this.y * (dt);
+    
+    // assures player is confined to canvas
+    if (this.x < -10) {
+        this.x = 200;
+        this.y = 400;
+        }
+    else if (this.x > 450) {
+        this.x = 200; 
+        this.y = 400;
+        }
+    else if (this.y < 40) {
+        this.x = 200; 
+        this.y = 400; 
+        }
+    else if (this.y > 400) {
+        this.x = 200; 
+        this.y = 400; 
+        }
+
+}; 
+
+Player.prototype.render = function() {
+   ctx.drawImage(Resources.get(this.sprite), this.x, this.y); 
+}; 
+
+Player.prototype.handleInput = function(keys) {
+    switch(keys) {
+        case 'left':
+            this.x = this.x - 103; 
+            break; 
+        case 'up':
+            this.y = this.y - 83; 
+            break; 
+        case 'right':
+            this.x = this.x + 103;
+            break; 
+        case 'down':
+            this.y = this.y + 83;
+            break; 
+    }; 
+    
+}; 
 
 // Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+var enemy1 = new Enemy(-50, 200, 300);
+var enemy2 = new Enemy(-300, 100, 100);
+var enemy3 = new Enemy(-200, 200, 50);
+var enemy4 = new Enemy(-150, 150, 200);
+var enemy5 = new Enemy(-50, 70, 270);
 
+// Place all enemy objects in an array called allEnemies
+var allEnemies = [];
+    allEnemies.push(enemy1, enemy2, enemy3, enemy4, enemy5);
+
+// Place the player object in a variable called player
+var player = new Player(200, 400); 
+
+//Check collision between player and enemies
+Enemy.prototype.checkCollisions = function(player) {
+    var playerHit = {
+        x: Player.x,
+        y: Player.y,
+        width: Player.w,
+        height: Player.h 
+    }
+ 
+    var enemyHit = {
+        x: this.x,
+        y: this.y,
+        width: this.w,
+        height: this.h
+    }
+ 
+    for (var i = 0; i < allEnemies.length; i++) {
+         if (enemyHit.x < playerHit.x + playerHit.width &&
+               enemyHit.x + enemyHit.width > playerHit.x &&
+               enemyHit.y < playerHit.y + playerHit.height &&
+               enemyHit.height + enemyHit.y > playerHit.y) {
+                    this.reset()// collision detected!
+            }
+        }
+    
+}; 
 
 
 // This listens for key presses and sends the keys to your
